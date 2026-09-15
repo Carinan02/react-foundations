6fbe6323714ca0496bd91748e1ebf72d0488af88
Carinan02 <141795962+Carinan02@users.noreply.github.com>
2026-09-15 00:11:28 +0000

    customHook

---
+++ 12_customHooks/demo-app-1/src/App.tsx
@@ -0,0 +1,14 @@
+import Header from './Header'
+import ImagePanel from './ImagePanel'
+
+function App() {
+
+  return (
+    <>
+     <Header/>
+     <ImagePanel/>
+    </>
+  )
+}
+
+export default App
